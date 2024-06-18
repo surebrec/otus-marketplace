c1df923dc0151ae26d2186a33f845cde857404cf
kostas <id.surebrec@gmail.com>
2024-06-18 12:35:49 +0300

    -add subproject -add test -fix gitignore

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,13 @@
+pluginManagement {
+    val kotlinVersion: String by settings
+    plugins {
+        kotlin("jvm") version kotlinVersion
+    }
+}
+
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
 }
 rootProject.name = "otus-marketplace"
 
+include("m1l1-first")
